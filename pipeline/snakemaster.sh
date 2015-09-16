21b1c4ed32d017471fbca5ca583e8db4ffec7cd7
Jay Hesselberth <jay.hesselberth@gmail.com>
2015-09-16 08:08:02 -0600

    store logs in workdir

--- pipeline/snakemaster.sh
+++ pipeline/snakemaster.sh
@@ -1,10 +1,15 @@
 #! /usr/bin/env bash
 
 #BSUB -J snakemaster
-#BSUB -o log/snakemaster_log.%J.out
-#BSUB -e log/snakemaster_log.%J.err
+#BSUB -o results-twoprime/log/snakemaster_log.%J.out
+#BSUB -e results-twoprime/log/snakemaster_log.%J.err
 
 set -o nounset -o pipefail -o errexit -x
+
+workdir="results-twoprime"
+if [[ ! -d $workdir/log ]]; then
+    mkdir -p $workdir/log
+fi
 
 args=' -q normal -n {threads} -o {log}.out -e {log}.err -J {params.job_name}'
 snakemake --drmaa "$args" --jobs 32 --configfile ribometh-seq-config.yml
